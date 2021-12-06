805c425b8f4a44d874ebc5e84d772aa7894bf44c
qverkk <membersy@gmail.com>
2021-12-06 23:08:55 +0100

    Finish part 2 of day 6

--- src/com/qverkk/aoc/day6/Day06.kt
+++ src/com/qverkk/aoc/day6/Day06.kt
@@ -22,17 +22,22 @@ data class Lanternfish(
     }
 }
 
+
+fun breed(fishes: List<Long>): List<Long> {
+    return fishes.mapIndexed { index, _ ->
+        when(index) {
+            6 -> fishes[0] + fishes[7]
+            8 -> fishes[0]
+            else -> fishes[index + 1]
+        }
+    }
+}
+
 fun main() {
     fun parseInitialLanternFishes(input: String): MutableList<Lanternfish> {
         return input.split(",")
             .map { Lanternfish(it.toInt()) }
             .toMutableList()
-    }
-
-    fun daysTillNewBirth(input: StringBuilder): Int {
-        return input.splitToSequence(",")
-            .filter { it.isNotEmpty() }
-            .minOf { it.toInt() } + 1
     }
 
     fun part1(input: List<String>, days: Int): Int {
@@ -54,16 +59,26 @@ fun main() {
         return fishes.size
     }
 
-    fun part2(input: List<String>): Int {
-        return 0
+    fun part2(input: List<String>, days: Int): Long {
+        val fishesAndDays = input.first()
+            .split(",")
+            .map { it.toInt() }
+            .groupingBy { it }
+            .eachCount()
+            .let { fishesWithinADay -> List(9) { fishesWithinADay[it]?.toLong() ?: 0L } }
+
+        return generateSequence(fishesAndDays, ::breed)
+            .take(days + 1)
+            .last()
+            .sum()
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("com/qverkk/aoc/day6/Day06_test")
     check(part1(testInput, 80) == 5934)
-    // check(part1(testInput, 256) == 26984457539)
+    check(part2(testInput, 256) == 26984457539)
 
     val input = readInput("com/qverkk/aoc/day6/Day06")
     println(part1(input, 80))
-    println(part2(input))
+    println(part2(input, 256))
 }
