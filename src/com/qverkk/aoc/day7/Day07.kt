db52a5cf3c0b511f5f9fc150b8a6529e868ab539
qverkk <membersy@gmail.com>
2021-12-07 16:48:01 +0100

    Finish puzzle 2 of day 7

--- src/com/qverkk/aoc/day7/Day07.kt
+++ src/com/qverkk/aoc/day7/Day07.kt
@@ -23,14 +23,25 @@ fun main() {
         return possibilities.minOf { it }
     }
 
-    fun part2(input: List<Int>): Long {
-        return 0
+    fun part2(input: List<Int>): Int {
+        val lastNumber = input.maxOf { it }
+        val possibilities = mutableListOf<Int>()
+        for (i in 0..lastNumber) {
+            possibilities.add(
+                input.sumOf { number ->
+                    val requiredSteps = abs(i - number)
+                    val fuelUsed = generateSequence(1) { it + 1 }.take(requiredSteps).sum()
+                    fuelUsed
+                }
+            )
+        }
+        return possibilities.minOf { it }
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("com/qverkk/aoc/day7/Day07_test").first().split(",").map { it.toInt() }
     check(part1(testInput) == 37)
-    // check(part2(testInput, 256) == 26984457539)
+    check(part2(testInput) == 168)
 
     val input = readInput("com/qverkk/aoc/day7/Day07").first().split(",").map { it.toInt() }
     println(part1(input))
